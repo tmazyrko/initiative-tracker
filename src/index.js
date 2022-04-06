6e2a6c09709a95e6dbd24b0eaac8f6ef687e5120
tmazyrko <tmazyrko@gmail.com>
2022-04-06 18:44:22 -0400

    index render setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/App';
 
+ReactDOM.render(
+    <Provider>
+        <BrowserRouter>
+            <Route path="/" exact component={App} />
+        </BrowserRouter>
+    </Provider>,
+    document.querySelector('#root')
+);
