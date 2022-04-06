f2ade0e6e5b12507d43e59552c6e34543cea1a7c
tmazyrko <tmazyrko@gmail.com>
2022-04-06 19:52:01 -0400

    Add function for mapping state to props

--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class TeamList extends React.Component {
     render() {
+        console.log(this.props);
         return(
             <h2>Team List</h2>
         );
     }
 }
 
-export default TeamList;
+const mapStateTopProps = state => {
+    return {
+        players: state.characters.players,
+        enemies: state.characters.enemies,
+    };
+};
+
+export default connect(mapStateTopProps)(TeamList);
+// connect returns a function which is immediately called, and to which TeamList is passed
