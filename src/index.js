0ecfbf6d6fd533f525451385e14dc980ba2a51f2
tmazyrko <tmazyrko@gmail.com>
2022-04-06 18:42:07 -0400

    More imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter, Router } from 'react-router-dom';
 
 import App from './components/App';
+
