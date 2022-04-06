06aacfe98ca895cbc3debee6bc6b7cfc51a34f28
tmazyrko <tmazyrko@gmail.com>
2022-04-06 19:24:12 -0400

    Create InitiativeList component

---
+++ src/components/InitiativeList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InitiativeList extends React.Component {
+    render() {
+        return(
+            <h2>Initiative List</h2>
+        );
+    }
+}
+
+export default InitiativeList;
